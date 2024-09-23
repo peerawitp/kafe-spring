771152e55edf855c8c3f9188a9882437a127dc3e
Peerawit Pharkdeepinyo <peerawitp@protonmail.com>
2024-09-23 17:19:18 +0700

    feat(auth): add authentication and validation

--- src/main/java/ku/cs/kafe/controller/SignupController.java
+++ src/main/java/ku/cs/kafe/controller/SignupController.java
@@ -1,10 +1,12 @@
 package ku.cs.kafe.controller;
 
-import ku.cs.kafe.entity.Member;
+import jakarta.validation.Valid;
+import ku.cs.kafe.request.SignupRequest;
 import ku.cs.kafe.service.SignupService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,20 +20,24 @@ public class SignupController {
 
 
     @GetMapping("/signup")
-    public String getSignupPage() {
+    public String getSignupPage(Model model) {
+        model.addAttribute("signupRequest", new SignupRequest());
         return "signup"; // return หน้าฟอร์ม signup.html
     }
 
 
     @PostMapping("/signup")
-    public String signupUser(@ModelAttribute Member user, Model model) {
+    public String signupUser(@Valid SignupRequest user, BindingResult result, Model model) {
+        if (result.hasErrors())
+            return "signup";
+
         if (signupService.isUsernameAvailable(user.getUsername())) {
             signupService.createUser(user);
             model.addAttribute("signupSuccess", true);
         } else {
             model.addAttribute("signupError", "Username not available");
         }
-        // return หน้าฟอร์ม signup.html เช่นกัน แต่จะมี message ปรากฎ
+        model.addAttribute("signupRequest", new SignupRequest());
         return "signup";
     }
 }
--- src/main/java/ku/cs/kafe/service/SignupService.java
+++ src/main/java/ku/cs/kafe/service/SignupService.java
@@ -2,6 +2,8 @@ package ku.cs.kafe.service;
 
 import ku.cs.kafe.entity.Member;
 import ku.cs.kafe.repository.MemberRepository;
+import ku.cs.kafe.request.SignupRequest;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -13,21 +15,19 @@ public class SignupService {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+    @Autowired
+    private ModelMapper modelMapper;
 
     public boolean isUsernameAvailable(String username) {
         return repository.findByUsername(username) == null;
     }
 
-    public void createUser(Member user) {
-        Member record = new Member();
-        record.setName(user.getName());
-        record.setUsername(user.getUsername());
+    public void createUser(SignupRequest user) {
+        Member record = modelMapper.map(user, Member.class);
         record.setRole("ROLE_USER");
-
 
         String hashedPassword = passwordEncoder.encode(user.getPassword());
         record.setPassword(hashedPassword);
-
 
         repository.save(record);
     }
